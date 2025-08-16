58e45624fbf1eb303824677d280e20d383cf9ba3
RomanSapunGit <sapunroma23@gmail.com>
2025-08-16 20:44:15 +0200

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -138,6 +138,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': None,
-    'PAGE_SIZE': None
+    "DEFAULT_PAGINATION_CLASS": None,
+    "PAGE_SIZE": None
 }
